f98f2d7fa422da256827517467a33d3bf16e9ab4
duc-beluga <dnguyen416@gatech.edu>
2023-01-25 23:38:55 -0500

    Set up Schemas and Frontend Setup

--- backend/schemas/index.js
+++ backend/schemas/index.js
@@ -1,1 +1,13 @@
-export const schemaTypes = []
+import abouts from "./abouts";
+import brands from "./brands";
+import testimonials from "./testimonials";
+import contact from "./contact"
+import experiences from "./experiences"
+import skills from "./skills"
+import workExperience from "./workExperience"
+import works from "./works"
+
+
+export const schemaTypes = [
+    testimonials, abouts, brands, contact, experiences, skills, workExperience, works
+]
